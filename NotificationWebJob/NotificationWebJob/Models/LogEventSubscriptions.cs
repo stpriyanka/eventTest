c2a0ebdd4580de0815dd98b56dc96c5c3a31bbef
pri <stpriyanka2011@yahoo.com>
2016-02-16 10:05:53 +0100

    deleted connection string and mandril api key to push on git

--- NotificationWebJob/NotificationWebJob/Models/LogEventSubscriptions.cs
+++ NotificationWebJob/NotificationWebJob/Models/LogEventSubscriptions.cs
@@ -13,4 +13,21 @@ namespace NotificationWebJob.Models
 
 		public Enum.ObjectTypeOfEvent ObjectTypeOfEvent { get; set; }
 	}
+	//public class Notification
+	//{
+
+	//	public Guid Id { get; set; }
+
+	//	//public string UserWhoSubscribed { get; set; }
+
+	//	public Enum.EventType EventType { get; set; }
+
+	//	public Enum.ObjectTypeOfEvent ObjectTypeOfEvent { get; set; }
+
+
+	//	public DateTime  DateTime.Now { get; set; }
+	//	public DateTime seennAt { get; set; }
+
+
+	//}
 }
